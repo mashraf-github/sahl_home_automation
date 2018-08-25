903d85d86c693d4020ea56788839115f8dc488d1
mahsraf-github <39669066+mahsraf-github@users.noreply.github.com>
2018-08-25 15:22:07 +0200

    First commit from eclipse

---
+++ sahl_home_automation/src/main/java/sahl/home_automation/sahl_home_automation/SahlHomeAutomationApplication.java
@@ -0,0 +1,12 @@
+package sahl.home_automation.sahl_home_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahlHomeAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahlHomeAutomationApplication.class, args);
+	}
+}
---
+++ sahl_home_automation/src/test/java/sahl/home_automation/sahl_home_automation/SahlHomeAutomationApplicationTests.java
@@ -0,0 +1,16 @@
+package sahl.home_automation.sahl_home_automation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SahlHomeAutomationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
